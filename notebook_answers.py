930be4c948b34d073266140da33c635b7de82c8e
Julia Galstad <julia.galstad@gmail.com>
2016-07-26 21:07:32 -0500

    moved explanation of justification of pseudocount use
    to the notebook, and out of notebook_answers.py

--- notebook_answers.py
+++ notebook_answers.py
@@ -13,9 +13,7 @@ hints[11.7] = "Our training set contains 400 spams and 425 non-spams."
 hints[12] = "How many total non-spam messages are there? Of those, how \nmany contain the word 'hodor'?"
 hints[12.5] = "You'll need to look at winter_dict to find how many non-spam\nemails contain the word 'hodor'."
 hints[12.7] = "Our training set contains 400 spams and 425 non-spams."
-hints[13.7] = "Reread the answer for 13.5 -- summing to 1 is an important \nproperty that we don't have anymore if we don't divide\n by the denomiator."
-hints[17.1] = "Try: \n winter_word_probabilities_2 = word_probabilities(winter_dict, 400, 425)\nspam_probability_prototype(winter_word_probabilities_2, 'witch red winter dragon')"
-hints[17] = "This will be similar to what we did previously. First, use this new \nfunction word_probabilities to calculate conditional \nprobabilities. Then use spam_probability_prototype to calculate the \nestimated probability the message is spam."
+
 
 answers = {}
 answers[0] = 42
@@ -27,13 +25,11 @@ answers[9] = "George R. R. Martin wrote these sentences to begin the book \n'A G
 answers[10] = "There are 5 words in our vocabulary, which are exactly the \nkeys of the dictionary:" + " 'hodor', 'winter', 'witch', 'red', 'dragon'. "
 answers[11] = 0.25
 answers[12] = 400/425
-answers[13] = 'We classify it as not spam because we estimated that the \nprobability of this message being spam as less than 8%.'
-answers[13.5] = "In the general Naive Bayes, we want to pick the hypothesis\nthat results in the highest probability. (That's what \nargmax does.) Since we only have two hypotheses, 'The message is spam.' and 'The message is not spam.', their probabilities must sum to 1. Whichever one has a probability greater than 0.5 will naturally have a probability greater than the other one. This means we only need to calculate one of the two probabilities. You could investigate this yourself by changing the return statement of the function spam_probability_prototype to:\n return message_prob_if_spam / (message_prob_if_spam + message_prob_if_not_spam), message_prob_if_not_spam / (message_prob_if_spam + message_prob_if_not_spam) \n "
-answers[13.7] = "No. If we don't divide by the denominator, we would have\nto calculate P(Spam|message) and P(not Spam|message), and\ncheck to see which one is bigger. " 
-answers[14] = 'We classify it as spam because we estimated that the \nprobability it is spam is greater than 97%.'
-answers[15] = "Our classifier is absolutely certain this message isn't spam."
+
+answers[14] = 'We classify it as spam because we estimated that the \nprobability it is spam (97%) is greater than 50%.'
+answers[15] = "Our classifier is absolutely certain this message isn't spam.\nThis is suspicious!"
 answers[16] = "In our training set, the word 'dragon' only appeared in \nmessages marked as 'not spam.' Our model thinks that P(S|dragon) = 0.\nSince we are multiplying probabilities together in our calculation, this 0 \nannihilated all the other probabilities."
-answers[17] = "With an estimated probability of about 66%, the message is\nclassified as spam."
+answers[17] = "With an estimated probability of about 85% (which is greater than 50%)\nthe message is classified as 'spam.'' \nThis is a very different answer than classifying the same message as \n'not spam' with absolute certainty!"
 
 def answer(number):
 	return answers.get(number, 'Sorry, ' + str(number) + ' is an invalid question number.')
